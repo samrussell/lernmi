311ba356bc38a78ac0ce855d140009d5a5ff332a
Sam Russell <sam.h.russell@gmail.com>
2016-03-26 11:36:56 +1300

    link layer test

--- spec/link_layer_spec.rb
+++ spec/link_layer_spec.rb
@@ -1,13 +1,30 @@
 require 'spec_helper'
+require './lib/neuron'
 require './lib/link'
 require './lib/neuron_layer'
 require './lib/link_layer'
 
 describe LinkLayer do
+  let(:input_neuron_1) { instance_double(Neuron) }
+  let(:input_neuron_2) { instance_double(Neuron) }
+  let(:input_bias_neuron) { instance_double(Neuron) }
+  let(:output_neuron_1) { instance_double(Neuron) }
+  let(:output_neuron_2) { instance_double(Neuron) }
+  let(:output_bias_neuron) { instance_double(Neuron) }
   let(:input_neuron_layer) { instance_double(NeuronLayer) }
   let(:output_neuron_layer) { instance_double(NeuronLayer) }
   let(:link_layer) { LinkLayer.new(input_neuron_layer, output_neuron_layer) }
 
-  xit "connects the neuron layers" do
+  it "creates links between the neurons" do
+    allow(input_neuron_layer).to receive(:all_neurons).and_return([input_bias_neuron, input_neuron_1, input_neuron_2])
+    allow(output_neuron_layer).to receive(:learning_neurons).and_return([output_neuron_1, output_neuron_2])
+    expect(Link).to receive(:new).with(anything, anything, input_bias_neuron, output_neuron_1).and_call_original
+    expect(Link).to receive(:new).with(anything, anything, input_bias_neuron, output_neuron_2).and_call_original
+    expect(Link).to receive(:new).with(anything, anything, input_neuron_1, output_neuron_1).and_call_original
+    expect(Link).to receive(:new).with(anything, anything, input_neuron_1, output_neuron_2).and_call_original
+    expect(Link).to receive(:new).with(anything, anything, input_neuron_2, output_neuron_1).and_call_original
+    expect(Link).to receive(:new).with(anything, anything, input_neuron_2, output_neuron_2).and_call_original
+
+    link_layer
   end
 end
